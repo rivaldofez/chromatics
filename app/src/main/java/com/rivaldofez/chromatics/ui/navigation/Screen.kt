d548af6685aeabe687214388a7885722338231aa
rivaldofez <rivaldofez@gmail.com>
2023-10-06 00:40:55 +0700

    add screen item profile

--- app/src/main/java/com/rivaldofez/chromatics/ui/navigation/Screen.kt
+++ app/src/main/java/com/rivaldofez/chromatics/ui/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.rivaldofez.chromatics.ui.navigation
 sealed class Screen(val route: String) {
     object Game: Screen("game")
     object Leaderboard: Screen("leaderboard")
+    object Profile: Screen("profile")
 }
